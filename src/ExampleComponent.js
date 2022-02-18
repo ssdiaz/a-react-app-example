44f44002cdd8ce6f6702c96c3fe572af7f869096
Samantha Diaz <samidiaz3@gmail.com>
2022-02-18 13:03:21 -0800

    commit tree d202eb77b4642f074bbc44c00975650eb7c4a237

--- src/ExampleComponent.js
+++ src/ExampleComponent.js
@@ -6,6 +6,7 @@ class ExampleComponent extends Component {
     return (
       <div className="example">
         <img src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif" alt="a deeply impressed man, perhaps thinking 'whoa'"/>
+        <img src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif" alt="a deeply impressed man, perhaps thinking 'whoa'"/>
         <p>Whoa!</p>
       </div>
     );
